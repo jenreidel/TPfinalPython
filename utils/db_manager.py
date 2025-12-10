232e585b6922183dd473113b77b3284c3b72f907
Jenisse Reidel <jeyula.lp@gmail.com>
2025-12-10 00:07:11 -0300

    push initial

---
+++ utils/db_manager.py
@@ -0,0 +1,2 @@
+import sqlite3
+from utils.helpers import imprimir_error
---
+++ utils/helpers.py
@@ -0,0 +1,46 @@
+import os
+from colorama import init, Fore, Style
+
+# Iniciamos colorama
+init(autoreset=True)
+
+def limpiar_pantalla():
+    os.system('cls' if os.name == 'nt' else 'clear')
+
+def imprimir_titulo(texto):
+    print(f"\n{Fore.CYAN}{Style.BRIGHT}==={texto.upper()}==={Style.RESET_ALL}")
+
+def imprimir_error(texto):
+    print(f"{Fore.RED} Error: {texto}{Style.RESET_ALL}")
+
+def imprimir_exito(texto):
+    print(f"{Fore.GREEN} {texto}{Style.RESET_ALL}")
+
+def validar_input_string(prompt):
+    while True:
+        dato = input(f"{Fore.YELLOW}{prompt}: {Style.RESET_ALL}").strip()
+        if dato:
+            return dato
+        imprimir_error("El campo no puede estar vacío")
+
+def validad_input_float(prompt):
+    while True:
+        try:
+            dato = float(input(f"{Fore.YELLOW}{prompt}: {Style.RESET_ALL}"))
+            if dato >= 0:
+                return dato
+            else:
+                imprimir_error("El número tiene que ser positivo")
+        except ValueError:
+            imprimir_error("El campo tiene que ser un número válido")
+
+def validar_input_int(prompt):
+    while True:
+        try:
+            dato = int(input(f"{Fore.YELLOW}{prompt}: {Style.RESET_ALL}"))
+            if dato >= 0:
+                return dato
+            else:
+                imprimir_error("El número tiene que ser positivo")
+        except ValueError:
+            imprimir_error("El campo tiene que ser un número válido")
